86f1ad71adbfc0a5f9342f41321be9340f928e50
hmji <hmji@nansoft.co.kr>
2021-08-11 16:10:06 +0900

    Add project files.

---
+++ sftptest/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace sftptest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(23, 26);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(117, 74);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(169, 26);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(107, 74);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ sftptest/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sftptest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //https://csharp.hotexamples.com/examples/Renci.SshNet/SftpClient/CreateDirectory/php-sftpclient-createdirectory-method-examples.html
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(Guid.NewGuid());
+
+            var ci = new ConnectionInfo("192.168.0.133", "loccitance", new PasswordAuthenticationMethod("loccitance", "0152"));
+
+            using (var sftp = new SftpClient(ci))
+            {
+                // SFTP 서버 연결
+                sftp.Connect();
+
+                // 현재 디렉토리 내용 표시
+                foreach (SftpFile f in sftp.ListDirectory("."))
+                {
+                    Console.WriteLine(f.Name);
+                }
+
+                // SFTP 다운로드
+                //using (var outfile = File.Create("HelloWorld.txt"))
+                //{
+                //    sftp.DownloadFile("./HelloWorld.txt", outfile);
+                //}
+
+                // SFTP 업로드
+                using (var infile = File.Open("HelloWorld.txt", FileMode.Open))
+                {
+                    if(!sftp.Exists("/RECV/20210810"))
+                    {
+                        sftp.CreateDirectory("/RECV/20210810");
+                    }
+                    //sftp.ChangeDirectory(ConstFields.TEMP_PRINT_DIRECTORY);
+                    sftp.UploadFile(infile, "/RECV/20210810/HelloWorld.txt");
+                }
+
+                sftp.Disconnect();
+            }
+        }
+    }
+}
